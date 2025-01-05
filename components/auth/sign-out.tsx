f8db6cfda814caf6078ea750551c9e1a75466a39
Ryuugamine <yoerdanatan@gmail.com>
2025-01-05 08:49:56 +0700

    chore : change signout button to client side

--- components/auth/sign-out.tsx
+++ components/auth/sign-out.tsx
@@ -1,19 +1,14 @@
+"use client";
 import { signOut } from "@/auth";
 
 export function SignOut() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signOut();
-      }}
+    <button
+      className="border rounded-lg p-2 text-gray-400 hover:border-white border-gray-400 hover:text-white transition-all duration-300 font-semibold"
+      type="submit"
+      onClick={() => signOut()}
     >
-      <button
-        className="border rounded-lg p-2 text-gray-400 hover:border-white border-gray-400 hover:text-white transition-all duration-300 font-semibold"
-        type="submit"
-      >
-        Sign Out
-      </button>
-    </form>
+      Sign Out
+    </button>
   );
 }
